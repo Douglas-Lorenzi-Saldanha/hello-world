c9278a04c7cf913ed05b324128f345a794dfde5c
Douglas-Lorenzi-Saldanha <103324394+Douglas-Lorenzi-Saldanha@users.noreply.github.com>
2022-05-11 17:45:47 -0300

    Update helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -8,4 +8,12 @@
   $result = 0;
   $texto = '';
   $txtedit = '';
+
+  $num1 = readline('Digite o num1: ');
+  $num2 = readline('Digite o num2: ');  
+
+  echo "Soma dos dois números: ";
+  echo "Subtração dos dois números: ";
+  echo "Multiplicação dos dois números: ";
+  echo "Divisão dos dois números: ";
 ?>
