edaa679f0c6eb535d59e34aaf72a7af0ca762b64
Douglas-Lorenzi-Saldanha <103324394+Douglas-Lorenzi-Saldanha@users.noreply.github.com>
2022-05-11 12:59:38 -0300

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,5 +2,14 @@
 
 int main() {
     std::cout << "Hello World!";
+    
+    int num1 = 0
+    int num2 = 0
+    int num3 = 0
+    int num4 = 0
+    int result = 0
+    string texto = '';
+    string txtedit = '';
+    
     return 0;
 }
