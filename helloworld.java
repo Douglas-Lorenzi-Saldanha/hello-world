6743f615e13970c6c3e926158c0802cdc850e892
Douglas-Lorenzi-Saldanha <103324394+Douglas-Lorenzi-Saldanha@users.noreply.github.com>
2022-05-11 17:40:04 -0300

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,12 @@
     public static void main(String[] args) {
         System.out.println("Hello World!");
      
-        
+        int num1 = 0
+        int num2 = 0
+        int num3 = 0
+        int num4 = 0
+        int result = 0
+        string texto = '';
+        string txtedit = '';        
     }
  }   
