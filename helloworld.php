1c4ae0129eccf75e9ed5199399c15877ec796869
Douglas-Lorenzi-Saldanha <103324394+Douglas-Lorenzi-Saldanha@users.noreply.github.com>
2022-05-11 17:56:15 -0300

    Update helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -1,19 +1,37 @@
-<?php
-  echo "Hello World";
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php
+      echo "Hello World";
 
-  $num1 = 0;
-  $num2 = 0;
-  $num3 = 0;
-  $num4 = 0;
-  $result = 0;
-  $texto = '';
-  $txtedit = '';
+      $num1 = 0;
+      $num2 = 0;
+      $num3 = 0;
+      $num4 = 0;
+      $result = 0;
+      $texto = '';
+      $txtedit = '';
 
-  $num1 = readline('Digite o num1: ');
-  $num2 = readline('Digite o num2: ');  
+      $num1 = readline('Digite o num1: ');
+      $num2 = readline('Digite o num2: ');  
 
-  echo "Soma dos dois números: ";
-  echo "Subtração dos dois números: ";
-  echo "Multiplicação dos dois números: ";
-  echo "Divisão dos dois números: ";
-?>
+      $result = $num1 + $num2;
+    
+      echo "Soma dos dois números: " . $result . "<br>";
+
+      $result = $num1 - $num2;
+
+      echo "Subtração dos dois números: " . $result . "<br>";
+
+      $result = $num1 * $num2;
+
+      echo "Multiplicação dos dois números: " . $result . "<br>";
+
+      $result = $num1 / $num2;
+
+      echo "Divisão dos dois números: " . $result . "<br>";
+    ?>
+  </body>
+</html>
