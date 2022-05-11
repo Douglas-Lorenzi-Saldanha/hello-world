04c2cb9a205c671255c9c60d4e99a40e4a2515d3
Douglas-Lorenzi-Saldanha <103324394+Douglas-Lorenzi-Saldanha@users.noreply.github.com>
2022-05-11 17:32:44 -0300

    Update helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -1,9 +1,9 @@
-echo "Hello World"
+echo "Hello World";
 
-    $num1 = 0
-    $num2 = 0
-    $num3 = 0
-    $num4 = 0
-    $result = 0
-    $texto = '';
-    $txtedit = '';
+$num1 = 0;
+$num2 = 0;
+$num3 = 0;
+$num4 = 0;
+$result = 0;
+$texto = '';
+$txtedit = '';
