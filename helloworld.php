d88248930bcb94850fb0638754b338feec8efdf4
Douglas-Lorenzi-Saldanha <103324394+Douglas-Lorenzi-Saldanha@users.noreply.github.com>
2022-05-12 10:00:46 -0300

    Update helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -4,8 +4,10 @@
   </head>
   <body>
     <?php
+      //Mensagem inicial
       echo "Hello World";
 
+      //Variaveis
       $num1 = 0;
       $num2 = 0;
       $num3 = 0;
@@ -14,23 +16,32 @@
       $texto = '';
       $txtedit = '';
 
+      //Recebimento de dados
       $num1 = readline('Digite o num1: ');
       $num2 = readline('Digite o num2: ');  
 
+      //Soma
       $result = $num1 + $num2;
-    
+
+      //Mensagem com o resultado da soma.
       echo "Soma dos dois números: " . $result . "<br>";
 
+      //Subtração
       $result = $num1 - $num2;
 
+      //Mensagem com o resultado da subtração.
       echo "Subtração dos dois números: " . $result . "<br>";
 
+      //Multiplicação
       $result = $num1 * $num2;
 
+      //Mensagem com o resultado da multiplicação.
       echo "Multiplicação dos dois números: " . $result . "<br>";
 
+      //Divisão
       $result = $num1 / $num2;
 
+      //Mensagem com o resultado da divisão.
       echo "Divisão dos dois números: " . $result . "<br>";
     ?>
   </body>
