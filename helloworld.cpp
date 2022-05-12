eab811156442aa7c8b67113ac2158bb8cc1272af
Douglas-Lorenzi-Saldanha <103324394+Douglas-Lorenzi-Saldanha@users.noreply.github.com>
2022-05-12 10:56:28 -0300

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,15 +1,50 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello World!";
-    
-    int num1 = 0
-    int num2 = 0
-    int num3 = 0
-    int num4 = 0
-    int result = 0
-    string texto = '';
-    string txtedit = '';
+    //Mensagem inicial
+    std::cout << "Hello World!" << endl;
+
+    //Variaveis
+    float num1 = 0;
+    float num2 = 0;
+    int num3 = 0;
+    int num4 = 0;
+    float result = 0;
+    string texto;
+    string txtedit;
+
+    //Recebimento de dados
+    std::cout << "Digite o num1: ";
+    cin >> num1;
+    std::cout << "Digite o num2: ";
+    cin >> num2;  
+
+    //Soma
+    result = num1 + num2;
+  
+    //Mensagem com o resultado da soma.
+    std::cout << "Soma dos dois números: " << result << endl;
+  
+    //Subtração
+    result = num1 - num2;
+  
+    //Mensagem com o resultado da subtração.
+    std::cout << "Subtração dos dois números: " << result << endl;
+  
+    //Multiplicação
+    result = num1 * num2;  
+  
+    //Mensagem com o resultado da multiplicação.
+    std::cout << "Multiplicação dos dois números: " << result << endl;
+  
+    //Divisão
+    result = num1 / num2;  
+  
+    //Mensagem com o resultado da divisão.
+    std::cout << "Divisão dos dois números: " << result << endl;  
     
     return 0;
 }
