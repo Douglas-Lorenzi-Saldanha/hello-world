6929b6e27aebf0d5c835e01ad33cc54206c26e2a
Douglas-Lorenzi-Saldanha <103324394+Douglas-Lorenzi-Saldanha@users.noreply.github.com>
2022-05-12 10:11:26 -0300

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -2,8 +2,8 @@
 console.log('Hello World!')
 
 //Variaveis
-const num1 = 0
-const num2 = 0
+let num1 = 0
+let num2 = 0
 const num3 = 0
 const num4 = 0
 let result = 0
@@ -11,13 +11,29 @@ const texto = '';
 let txtedit = '';
 
 //Recebimento de dados
-
+num1 = prompt('Digite o num1: ')
+num2 = prompt('Digite o num2: ')
 
 //Soma
+result = parseInt(num1) + parseInt(num2)
+
 //Mensagem com o resultado da soma.
+console.log("Soma dos dois números: " + result)
+
 //Subtração
+result = parseInt(num1) - parseInt(num2)
+
 //Mensagem com o resultado da subtração.
+console.log("Subtração dos dois números: " + result)
+
 //Multiplicação
+result = parseInt(num1) * parseInt(num2)
+
 //Mensagem com o resultado da multiplicação.
+console.log("Multiplicação dos dois números: " + result)
+
 //Divisão
+result = parseInt(num1) / parseInt(num2)
+
 //Mensagem com o resultado da divisão.
+console.log("Divisão dos dois números: " + result)
