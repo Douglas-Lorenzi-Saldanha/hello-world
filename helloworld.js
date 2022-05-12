3aaf3cfb18197cb72e619f17e1d1ff0ca5cd3545
Douglas-Lorenzi-Saldanha <103324394+Douglas-Lorenzi-Saldanha@users.noreply.github.com>
2022-05-12 10:05:00 -0300

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,5 +1,7 @@
+//Mensagem inicial
 console.log('Hello World!')
 
+//Variaveis
 const num1 = 0
 const num2 = 0
 const num3 = 0
@@ -7,3 +9,15 @@ const num4 = 0
 let result = 0
 const texto = '';
 let txtedit = '';
+
+//Recebimento de dados
+
+
+//Soma
+//Mensagem com o resultado da soma.
+//Subtração
+//Mensagem com o resultado da subtração.
+//Multiplicação
+//Mensagem com o resultado da multiplicação.
+//Divisão
+//Mensagem com o resultado da divisão.
