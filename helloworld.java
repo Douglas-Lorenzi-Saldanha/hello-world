ff4b2ff721ede91d1cf47a7c2e9984ca784bd1ab
Douglas-Lorenzi-Saldanha <103324394+Douglas-Lorenzi-Saldanha@users.noreply.github.com>
2022-05-13 18:48:34 -0300

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,13 +1,25 @@
  public class HelloWorld {   
     public static void main(String[] args) {
+        //Mensagem inicial
         System.out.println("Hello World!");
      
+        //Variaveis
         int num1 = 0
         int num2 = 0
         int num3 = 0
         int num4 = 0
         int result = 0
         string texto = '';
-        string txtedit = '';        
+        string txtedit = '';       
+     
+        //Recebimento de dados
+        //Soma
+        //Mensagem com o resultado da soma.
+        //Subtração
+        //Mensagem com o resultado da subtração.
+        //Multiplicação
+        //Mensagem com o resultado da multiplicação.
+        //Divisão
+        //Mensagem com o resultado da divisão.
     }
  }   
