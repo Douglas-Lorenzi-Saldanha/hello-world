e3e1bc0fbf868facba762c45d881218062ff4424
Douglas-Lorenzi-Saldanha <103324394+Douglas-Lorenzi-Saldanha@users.noreply.github.com>
2022-05-17 22:09:09 -0300

    Update helloworld.cs

--- helloworld.cs
+++ helloworld.cs
@@ -2,6 +2,47 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    //Mensagem inicial
+    Console.WriteLine("Hello World");
+
+    //Variaveis
+    double num1 = 0;
+    double num2 = 0;
+    int num3 = 0;
+    int num4 = 0;
+    double result = 0;
+    string texto = "";
+    string texto2 = "";
+
+    //Recebimento de dados
+    Console.Write("Digite o num1: ");
+    num1 = double.Parse(Console.ReadLine());
+    Console.Write("Digite o num2: ");
+    num2 = double.Parse(Console.ReadLine());
+
+    //Soma
+    result = num1 + num2;
+
+    //Mensagem com o resultado da soma.
+    Console.Write("Soma dos dois números: " + result + "\n");
+
+    //Subtração
+    result = num1 - num2;
+
+    //Mensagem com o resultado da subtração.
+    Console.Write("Subtração dos dois números: " + result + "\n");
+
+    //Multiplicação
+    result = num1 * num2;
+
+    //Mensagem com o resultado da multiplicação.
+    Console.Write("Multiplicação dos dois números: " + result + "\n");
+
+    //Divisão
+    result = num1 / num2;
+
+    //Mensagem com o resultado da divisão.
+    Console.Write("Divisão dos dois números: " + result + "\n");
+  
   }
 }
