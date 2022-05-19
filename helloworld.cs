8e9ae78adc2f547cbc1b824c101243681e5fcb77
Douglas-Lorenzi-Saldanha <103324394+Douglas-Lorenzi-Saldanha@users.noreply.github.com>
2022-05-19 09:31:33 -0300

    Update helloworld.cs

--- helloworld.cs
+++ helloworld.cs
@@ -52,10 +52,10 @@ class Program {
     
     //Teste de If e Else para ver qual é o número maior
     if(num3>num4){
-      num5 = num3
+      num5 = num3;
     }
     else{
-     num5 = num4
+     num5 = num4;
     }     
   
   }
